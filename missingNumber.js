2c1947f8943db3faa34bb14954f6f7bd2278cc57
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-31 11:55:21 +0500

    3nd commit

--- missingNumber.js
+++ missingNumber.js
@@ -4,15 +4,16 @@ let arr = [12, 13, 16, 19];
 let newArray = [];
 let firstValue = arr[0];
 let lastValue = arr[arr.length - 1];
-console.log(firstValue);
-console.log(lastValue);
 
+/* Creating a new array with all the numbers between the first and last value of the original array. */
 for (let i = firstValue; i <= lastValue; i++) {
   if (i >= firstValue && i <= lastValue) {
     newArray.push(i);
   }
 }
 let result = [];
+/* Checking if the item is not included in the original array. If it is not included, it will push the
+item and the index number to the result array. */
 newArray.forEach((item, i) => {
   if (!arr.includes(item)) {
     console.log(item);
