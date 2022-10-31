997e9b1f88b5609ca270c5a0cd89d0f505fae58c
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-31 13:45:16 +0500

    updated

---
+++ prototype.js
@@ -0,0 +1,38 @@
+Object.defineProperty(Array.prototype, "mySortingArray", {
+  value: function (compare) {
+    return [].concat(this).sort(compare);
+  },
+});
+
+Object.defineProperty(Array.prototype, "addAllNums", {
+  value: function (numsArray) {
+    let a = numsArray.reduce((a, b) => a + b, 0);
+    return a;
+  },
+});
+
+let num = [4, 6, 3, 2, 5, 7, 1, 8];
+
+console.log(num.mySortingArray());
+console.log(num.addAllNums(num));
+// built in Prototypes methods
+// fill()
+// reverse()
+// pop()
+// shift()
+// push()
+// unshift()
+// sort()
+// splice()
+// concat()
+// join()
+// slice()
+// includes()
+// indexOf()
+// map()
+// forEach()
+// reduce()
+// filter()
+// find()
+// every()
+// some()
