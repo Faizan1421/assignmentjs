16166a5b74cd61ad23a4737a0828fae88ede6f23
Faizan Tayyab <Faizan1421@gmail.com>
2022-10-31 12:01:02 +0500

    converted into function

--- missingNumber.js
+++ missingNumber.js
@@ -1,23 +1,27 @@
 let arr = [12, 13, 16, 19];
 
 // find missing numbers with their index number
-let newArray = [];
-let firstValue = arr[0];
-let lastValue = arr[arr.length - 1];
+function missingNumbers(arr) {
+  let newArray = [];
+  let firstValue = arr[0];
+  let lastValue = arr[arr.length - 1];
 
-/* Creating a new array with all the numbers between the first and last value of the original array. */
-for (let i = firstValue; i <= lastValue; i++) {
-  if (i >= firstValue && i <= lastValue) {
-    newArray.push(i);
+  /* Creating a new array with all the numbers between the first and last value of the original array. */
+  for (let i = firstValue; i <= lastValue; i++) {
+    if (i >= firstValue && i <= lastValue) {
+      newArray.push(i);
+    }
   }
+  let result = [];
+  /* Checking if the item is not included in the original array. If it is not included, it will push the
+  item and the index number to the result array. */
+  newArray.forEach((item, i) => {
+    if (!arr.includes(item)) {
+      console.log(item);
+      result.push(`${item} is missing on index ${i}`);
+    }
+  });
+  return result;
 }
-let result = [];
-/* Checking if the item is not included in the original array. If it is not included, it will push the
-item and the index number to the result array. */
-newArray.forEach((item, i) => {
-  if (!arr.includes(item)) {
-    console.log(item);
-    result.push(`${item} is missing on index ${i}`);
-  }
-});
-console.log(result.join("\n"));
+let ans = missingNumbers(arr).join("\n");
+console.log(ans);
