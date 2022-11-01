87bace472177de5ab94ac08ed275532377c560ac
Faizan Tayyab <Faizan1421@gmail.com>
2022-11-01 18:43:27 +0500

    updated

--- 2-prototype.js
+++ 2-prototype.js
@@ -1,3 +1,32 @@
+class Calculator {
+  addition(...nums) {
+    let res = nums.reduce((a, b) => a + b, 0);
+    return res;
+  }
+  multiply(...nums) {
+    let res = nums.reduce((a, b) => a * b);
+    return res;
+  }
+  divide(a, b) {
+    let res = a / b;
+    return res;
+  }
+  minus(a, b) {
+    let res = a - b;
+    return res;
+  }
+}
+let add = new Calculator();
+console.log(add.addition(2, 4, 6, 2));
+let multiply = new Calculator();
+console.log(multiply.multiply(2, 4, 6, 2));
+let divide = new Calculator();
+console.log(divide.divide(4, 2));
+let minus = new Calculator();
+console.log(minus.minus(6, 2));
+
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 Object.defineProperty(Array.prototype, "mySortingArray", {
   value: function (compare) {
     return [].concat(this).sort(compare);
